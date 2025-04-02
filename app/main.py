69a93a906ef56768d625a14e0c50d6271806b8f1
Roman Azhniuk <roman.azhniuk@gmail.com>
2025-04-02 23:55:53 +0300

    implement py-vector-class

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from typing import Union
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coordinate: float, y_coordinate: float) -> None:
+        self.x = round(x_coordinate, 2)
+        self.y = round(y_coordinate, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
